fcc7acbdaa0f8940cab6cbde585aa311984631ea
ParthGupta84616 <148619447+ParthGupta84616@users.noreply.github.com>
2024-04-10 21:01:55 +0530

    Adding more certificate

--- src/Components/Certificate.jsx
+++ src/Components/Certificate.jsx
@@ -4,9 +4,13 @@ import Python from '../assets/Python.png';
 import AWS from '../assets/AWS.png'; 
 import CTraining from '../assets/CTraining.png'; 
 import CyberSecurity from "../assets/CyberSecurity.jpg"
-import Energy from "../assets/Energy.png"
-import Cpp from "../assets/Cpp.png"
-import ReactCertificate from "../assets/React.png"
+import ADA from "../assets/ADA.png";
+import Energy from "../assets/Energy.png";
+import Cpp from "../assets/Cpp.png";
+// import ADA from "../assets/ADA.png";
+import ProblemSolving from "../assets/ProblemSolving.png";
+import DaysOfCode from "../assets/DaysOfCode.png";
+import ReactCertificate from "../assets/React.png";
 import CertificateNameCard from './CertificateNameCard';
 function Certificate() {
   const [First, setFirst] = useState(Python)
@@ -21,9 +25,19 @@ function Certificate() {
         className="container px-4 sm:py-5 relative overflow-x-auto"
       >
         <div className="flex gap-4 mt-6 font-medium whitespace-nowrap auto-scroll-container ">
+       
           <div className="flex-none w-1/2">
             <CertificateCard imgURL={First} title={"Python (Basic)"}/>
           </div>
+           <div className="flex-none w-1/2">
+            <CertificateCard imgURL={ADA} title={"ADA"}/>
+          </div>
+          <div className="flex-none w-1/2">
+            <CertificateCard imgURL={ProblemSolving} title={"Problem Solving"}/>
+          </div> 
+           <div className="flex-none w-1/2">
+            <CertificateCard imgURL={DaysOfCode} title={"100Days Of Code"}/>
+          </div> 
           <div className="flex-none w-1/2">
             <CertificateCard imgURL={AWS} title={"AWS Builder Online Series"}/>
           </div>
@@ -42,6 +56,7 @@ function Certificate() {
           <div className="flex-none w-1/2">
             <CertificateCard imgURL={ReactCertificate} title={"Python (Basic)"}/>
           </div>
+         
         </div>
       </div>
       <div className="container m-auto px-4 py-14">
@@ -58,6 +73,11 @@ function Certificate() {
         <button onClick={() => handleClick(Energy)}><CertificateNameCard Cirtificate={"Energy Literacy"} /></button>
         <button onClick={() => handleClick(CyberSecurity)}><CertificateNameCard Cirtificate={"Cyber Security"} /></button>
         <button onClick={() => handleClick(AWS)}><CertificateNameCard Cirtificate={"Builder Online Series"} /></button>
+    </div>
+    <div className="flex flex-col sm:flex-row gap-4 sm:gap-28 mt-12 w-[80%]">
+        <button onClick={() => handleClick(ADA)}><CertificateNameCard Cirtificate={"ADA"} /></button>
+        <button onClick={() => handleClick(ProblemSolving)}><CertificateNameCard Cirtificate={"Problem Solving"} /></button>
+        <button onClick={() => handleClick(DaysOfCode)}><CertificateNameCard Cirtificate={"100Days Of Code"} /></button>
     </div>
 </div>
 
